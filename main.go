35300c00e88efe048399a5dfa956508f430bde82
Leopoldo Caballero <git@pitakill.net>
2018-08-25 21:44:01 -0500

    Add version to the binary (#6)

--- main.go
+++ main.go
@@ -18,7 +18,14 @@ func main() {
 	dataFlag := flag.String("data", "https://cucodev.culturacolectiva.com/jsoncategory/", "URL for get the data (json) to add")
 	token := flag.String("token", "", "Token needed for make the petition")
 	environment := flag.String("environment", "dev", "Environment to make the petition {dev, staging}")
+	v := flag.Bool("v", false, "Print the version of the program")
+	version := flag.Bool("version", false, "Print the version of the program")
 	flag.Parse()
+
+	if *v || *version {
+		fmt.Printf("go-filler version %s\n", appVersion)
+		os.Exit(0)
+	}
 
 	if *token == "" {
 		flag.PrintDefaults()
