24f01674a6bf2ccb663f57346a937c8f6c347644
Leopoldo Caballero <git@pitakill.net>
2018-10-04 13:24:57 -0500

    0101000001000011/fix fill meta (#16)
    * Update version
    
    * Update creation of binaries, erase them first, then build them

--- make-binaries.sh
+++ make-binaries.sh
@@ -3,6 +3,14 @@
 LINUX_BINARY=go-filler-linux
 OSX_BINARY=go-filler-osx
 
+if [ -f $LINUX_BINARY ]; then
+  rm -v $LINUX_BINARY
+fi
+
+if [ -f $OSX_BINARY ]; then
+  rm -v $OSX_BINARY
+fi
+
 echo "Making Linux 64bit binary: ${LINUX_BINARY}"
 GOOS=linux GOARCH=amd64 go build -v -o $LINUX_BINARY
 
